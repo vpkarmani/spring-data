7a8ff3d37d1179ad7616abb22790bd16290838dc
Vijay Karmani <vpkarmani@sintecmedia.com>
2020-05-31 16:20:24 +0530

    spring data mongodb example

---
+++ spring-data-mongodb/src/test/java/com/vpkarmani/mongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpkarmani.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
